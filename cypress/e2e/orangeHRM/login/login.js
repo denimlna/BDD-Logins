1dc08edfdca0abde868b7d3e4f4638a472a74c31
denimlna <denimaulana758@gmail.com>
2024-10-24 16:12:16 +0700

    Initial commit

---
+++ cypress/e2e/orangeHRM/login/login.js
@@ -0,0 +1,32 @@
+import { Given, When,Then } from "@badeball/cypress-cucumber-preprocessor";
+import loginPage from "../../../pom/login.cy";
+import reset_pass from "../../../pom/reset_pass.cy";
+
+
+Given('I visit the URL', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    cy.wait(25000);
+  
+});
+
+When('I should see the homepage', () => {
+    loginPage.verifyLoginPage().should('have.text','Login');
+});
+
+When('I submit the Username', () => {
+    loginPage.inputUsername().type('Admin');
+});
+
+When('I submit the Password', () => {
+    loginPage.inputPassword().type('admin123');
+});
+
+Then('I click the button Login', () => {
+    loginPage.InterceptLogin();
+    loginPage.buttonSubmit().click();
+    
+});
+
+Then('I verify login success', () => {
+    loginPage.dashboardPage().should('have.text', 'Dashboard')
+});
